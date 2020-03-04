b06406337f66d8f7d676352b00fd5b7f20b977ba
rolypolyvg295 <rolypolyvg295@users.noreply.github.com>
2020-03-04 17:13:00 +0900

    추가 확인 다이얼로그 UI 생성

---
+++ app/app/src/main/java/com/example/expirationdateapp/FinalConfirmDialogFrament.java
@@ -0,0 +1,40 @@
+package com.example.expirationdateapp;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+public class FinalConfirmDialogFrament extends DialogFragment {
+    private View body = null;
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+
+        LayoutInflater inflater = getLayoutInflater();
+        body = inflater.inflate(R.layout.dialog_final_confirm, null);
+        builder.setView(body);
+        builder.setPositiveButton(R.string.button_add, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        builder.setNegativeButton(R.string.button_cancel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        return builder.create();
+    }
+}
