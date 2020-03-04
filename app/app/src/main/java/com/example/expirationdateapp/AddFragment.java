a71dfd593e2dbb4f9aedffd0c47b369ff2536c96
rolypolyvg295 <rolypolyvg295@users.noreply.github.com>
2020-03-05 07:57:40 +0900

    즐겨찾기 추가버튼 액션바에 추가

--- app/app/src/main/java/com/example/expirationdateapp/AddFragment.java
+++ app/app/src/main/java/com/example/expirationdateapp/AddFragment.java
@@ -3,6 +3,7 @@ package com.example.expirationdateapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
@@ -11,8 +12,12 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -26,12 +31,32 @@ public class AddFragment extends Fragment {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate (Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_add, container, false);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.add_toolbar, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.action_add_to_favorite){
+            Toast.makeText(getContext(), "Add to favorite", Toast.LENGTH_SHORT).show();
+            return true;
+        }else
+            return super.onOptionsItemSelected(item);
     }
 
     @Override
