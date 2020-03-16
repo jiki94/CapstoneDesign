dca22a69e293f20e406158b3047ddb05620f376e
rolypolyvg295 <rolypolyvg295@users.noreply.github.com>
2020-03-16 11:55:11 +0900

    카카오 관련 설정, ocr 테스트 액티비티 생성

---
+++ app/app/src/main/java/com/example/expirationdateapp/OCRTestingActivity.java
@@ -0,0 +1,14 @@
+package com.example.expirationdateapp;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class OCRTestingActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ocr_testing);
+    }
+}
