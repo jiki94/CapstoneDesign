ad041ba2efa4a93b8f7f2e4f09f853ee3757d400
rolypolyvg295 <rolypolyvg295@users.noreply.github.com>
2020-04-18 17:27:01 +0900

    유통기한 지난 탭에 정보 추가

--- app/app/src/main/java/com/example/expirationdateapp/viewing/ViewRecyclerViewAdapter.java
+++ app/app/src/main/java/com/example/expirationdateapp/viewing/ViewRecyclerViewAdapter.java
@@ -15,12 +15,18 @@ import com.example.expirationdateapp.R;
 import com.example.expirationdateapp.db.LocalDateConverter;
 import com.example.expirationdateapp.db.Product;
 
+import org.threeten.bp.LocalDate;
+import org.threeten.bp.Period;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 // 보기 Fragment 에서 사용
-public class ViewRecyclerViewAdapter extends RecyclerView.Adapter<ViewRecyclerViewAdapter.CustomViewHolder> {
+public class ViewRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    static private final int NORMAL_TYPE = 0;
+    static private final int OVERDUE_TYPE = 1;
+
     private Context context;
     private List<Product> data;
     private List<Product> filteredData;
@@ -29,17 +35,35 @@ public class ViewRecyclerViewAdapter extends RecyclerView.Adapter<ViewRecyclerVi
     private String filterString;
     private SortingType sortFlag;
 
-    class CustomViewHolder extends RecyclerView.ViewHolder{
+    class NormalViewHolder extends RecyclerView.ViewHolder{
         private TextView name;
         private TextView expiryDate;
         private Button delete;
 
-        CustomViewHolder(@NonNull View itemView) {
+        NormalViewHolder(@NonNull View itemView) {
             super(itemView);
 
-            name = itemView.findViewById(R.id.viewItem_text_name);
-            expiryDate = itemView.findViewById(R.id.viewItem_text_expiry_date);
-            delete = itemView.findViewById(R.id.viewItem_button_del);
+            name = itemView.findViewById(R.id.viewItemNormal_text_name);
+            expiryDate = itemView.findViewById(R.id.viewItemNormal_text_expiry_date);
+            delete = itemView.findViewById(R.id.viewItemNormal_button_del);
+        }
+    }
+
+    class OverdueViewHolder extends RecyclerView.ViewHolder{
+        private TextView name;
+        private TextView stored;
+        private TextView expiryDate;
+        private TextView daysPast;
+        private Button delete;
+
+        OverdueViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            name = itemView.findViewById(R.id.viewItemOverdue_text_name);
+            stored = itemView.findViewById(R.id.viewItemOverdue_text_stored);
+            expiryDate = itemView.findViewById(R.id.viewItemOverdue_text_expiry_date);
+            daysPast = itemView.findViewById(R.id.viewItemOverdue_text_past_date);
+            delete = itemView.findViewById(R.id.viewItemOverdue_button_del);
         }
     }
 
@@ -55,25 +79,67 @@ public class ViewRecyclerViewAdapter extends RecyclerView.Adapter<ViewRecyclerVi
 
     @NonNull
     @Override
-    public CustomViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View view = inflater.inflate(R.layout.item_view, parent, false);
-        return new CustomViewHolder(view);
+        View view;
+        RecyclerView.ViewHolder viewholder;
+        if (viewType == NORMAL_TYPE) {
+            view = inflater.inflate(R.layout.item_view_normal, parent, false);
+            viewholder= new NormalViewHolder(view);
+        }else if (viewType == OVERDUE_TYPE){
+            view = inflater.inflate(R.layout.item_view_overdue, parent, false);
+            viewholder= new OverdueViewHolder(view);
+        }else{
+            throw new IllegalArgumentException();
+        }
+        return viewholder;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull CustomViewHolder holder, int position) {
+    public int getItemViewType(int position) {
+        Product datum = filteredData.get(position);
+        if (datum.expiryDate.compareTo(LocalDate.now()) >= 0){
+            return NORMAL_TYPE;
+        }else
+            return OVERDUE_TYPE;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int position) {
         final Product datum = filteredData.get(position);
 
-        holder.name.setText(datum.name);
-        holder.expiryDate.setText(LocalDateConverter.localDateToString(datum.expiryDate));
+        switch (viewHolder.getItemViewType()){
+            case NORMAL_TYPE:
+                NormalViewHolder nHolder = (NormalViewHolder) viewHolder;
+                nHolder.name.setText(datum.name);
+                nHolder.expiryDate.setText(LocalDateConverter.localDateToString(datum.expiryDate));
 
-        holder.delete.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                listener.onDeletedClicked(datum);
-            }
-        });
+                nHolder.delete.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        listener.onDeletedClicked(datum);
+                    }
+                });
+                break;
+            case OVERDUE_TYPE:
+                OverdueViewHolder oHolder = (OverdueViewHolder) viewHolder;
+                oHolder.name.setText(datum.name);
+                oHolder.stored.setText(datum.stored.getStringId());
+                oHolder.expiryDate.setText(LocalDateConverter.localDateToString(datum.expiryDate));
+                oHolder.delete.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        listener.onDeletedClicked(datum);
+                    }
+                });
+
+                Period period = Period.between(datum.expiryDate, LocalDate.now());
+                int pasted = period.getDays();
+                oHolder.daysPast.setText(String.valueOf(pasted));
+                break;
+            default:
+                throw new IllegalArgumentException();
+        }
     }
 
     @Override
