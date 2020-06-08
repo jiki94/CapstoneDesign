071f9e34c4ee3f83ca3430707b5ef05ceb71d436
jiki94 <jiki94@naver.com>
2020-06-08 16:29:06 +0900

    게시글

---
+++ app/notice/Notice2/app/src/main/java/com/example/notice/activity/editor/EditorPresenter.java
@@ -0,0 +1,110 @@
+package com.example.notice.activity.editor;
+
+import androidx.annotation.NonNull;
+
+import com.example.notice.api.ApiClient;
+import com.example.notice.api.ApiInterface;
+import com.example.notice.model.Note;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class EditorPresenter {
+
+    private EditorView view;
+
+    public EditorPresenter(EditorView view){
+        this.view = view;
+    }
+
+     void saveNote(final String title, final String note, final int color) {
+
+        view.showProgress();
+
+        ApiInterface apiInterface = ApiClient.getApiClient()
+                .create(ApiInterface.class);
+        Call<Note> call = apiInterface.saveNote(title, note, color);
+
+        call.enqueue(new Callback<Note>() {
+            @Override
+            public void onResponse(@NonNull Call<Note> call, @NonNull Response<Note> response) {
+                view.hideProgress();
+
+                if(response.isSuccessful() && response.body() != null){
+                    Boolean success = response.body().getSuccess();
+                    if(success){
+                        view.onRequestSuccess(response.body().getMessage());
+                    }else{
+                        view.onRequestError(response.body().getMessage());
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<Note> call, @NonNull Throwable t) {
+                view.hideProgress();
+                view.onRequestError(t.getLocalizedMessage());
+
+            }
+        });
+    }
+
+    void updateNote(int id, String title, String note, int color){
+
+        view.showProgress();
+        ApiInterface apiInterface = ApiClient.getApiClient().create(ApiInterface.class);
+
+        Call<Note> call = apiInterface.updateNote(id, title, note, color);
+        call.enqueue(new Callback<Note>() {
+            @Override
+            public void onResponse(@NonNull Call<Note> call, @NonNull Response<Note> response) {
+                view.hideProgress();
+                if (response.isSuccessful() && response.body() != null){
+
+                    Boolean success = response.body().getSuccess();
+                    if(success){
+                        view.onRequestSuccess(response.body().getMessage());
+                    }else{
+                        view.onRequestError(response.body().getMessage());
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<Note> call, @NonNull Throwable t) {
+                view.hideProgress();
+                view.onRequestError(t.getLocalizedMessage());
+            }
+        });
+    }
+
+    void deleteNote(int id){
+
+        view.showProgress();
+        ApiInterface apiInterface = ApiClient.getApiClient().create(ApiInterface.class);
+        Call<Note> call = apiInterface.deleteNote(id);
+        call.enqueue(new Callback<Note>() {
+            @Override
+            public void onResponse(@NonNull Call<Note> call, @NonNull Response<Note> response) {
+                view.hideProgress();
+                if (response.isSuccessful() && response.body() != null){
+
+                    Boolean success = response.body().getSuccess();
+                    if (success){
+                        view.onRequestSuccess(response.body().getMessage());
+                    }else{
+                        view.onRequestError(response.body().getMessage());
+                    }
+
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<Note> call, @NonNull Throwable t) {
+                view.hideProgress();
+                view.onRequestError(t.getLocalizedMessage());
+            }
+        });
+    }
+}
---
+++ app/notice/Notice2/app/src/main/java/com/example/notice/activity/main/MainView.java
@@ -0,0 +1,12 @@
+package com.example.notice.activity.main;
+
+import com.example.notice.model.Note;
+
+import java.util.List;
+
+public interface MainView {
+    void showLoading();
+    void hideLoading();
+    void onGetResult(List<Note> notes);
+    void onErrorLoading(String message);
+}
